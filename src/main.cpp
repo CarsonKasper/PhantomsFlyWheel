b1a6a994112f97442c5c200aa0f850fb37a37c50
Carson Kasper <carson.akasper@gmail.com>
2024-02-02 17:22:58 -0700

    Hang function

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,35 @@
 #include "main.h"
-//"griffin is stupid"
+
 pros::Motor Arm(10);
 pros::Motor FlyWheel(2);
 pros::ADIDigitalOut PTOPiston('A');
 
-Drive chassis ({-14, -11, -15}, {16/*was 5*/, 13/*was 5*/, 8}, 1, 4.125, 600, 0.57142857142);
-
-int endgame = 0;
+Drive chassis ({-14, -11, -15}, {16, 13, 8}, 1, 4.125, 600, 0.57142857142);
 
 void Arm_Control() {
   while (true) {
     pros::delay(10);
-    if (endgame == 0) {
     while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-      Arm.move(127);
-    }
-    while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-      Arm.move(-80);
-    }
-    } 
-    else {
-      while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-      Arm.move(127);
+    Arm.move(127);
     }
     while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
       Arm.move(-127);
     }
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+      chassis.set_drive_brake(MOTOR_BRAKE_HOLD);
+      Arm.move(-127);
+      pros::delay(1000);
+      PTOPiston.set_value(true);
+      chassis.set_tank(-127, -127);
+      delay(1000);
+      chassis.set_tank(0,0);
+      Arm.brake();
+    }
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+      chassis.set_drive_brake(MOTOR_BRAKE_COAST);
+      PTOPiston.set_value(false);
     }
     Arm.brake();
-  }
-}
-
-void ActuatePTO () {
-  int check = 1;
-  while (true) {
-    pros::delay(200);
-    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-      if (check == 1) {
-        PTOPiston.set_value(true);
-        check = 0;
-        endgame = 1;
-      } else {
-        PTOPiston.set_value(false);
-        check = 1;
-        endgame = 0;
-      }
-    }      
   }
 }
 
@@ -108,8 +92,6 @@ void competition_initialize() {
 }
 
 void autonomous() {
- // pros::Task BangBangTask(BangBangLoop);
-
   chassis.reset_pid_targets(); 
   chassis.reset_gyro(); 
   chassis.reset_drive_sensor(); 
@@ -124,11 +106,8 @@ void opcontrol() {
   FlyWheel.set_brake_mode(MOTOR_BRAKE_COAST);
   pros::Task Control_Arm(Arm_Control);
   pros::Task Control_FlyWheel(FlyWheel_Control);
-  pros::Task PTO_Actuate(ActuatePTO);
   while (true) {
     chassis.arcade_standard(ez::SPLIT);
-
     pros::delay(ez::util::DELAY_TIME);
-
   }
 }
